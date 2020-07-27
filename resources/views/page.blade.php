a7f2c54911ec5c198d43dac73e86119b376ea9c8
edward01s2 <edward.01s2@gmail.com>
2020-07-27 14:11:31 -0500

    Fix privacy

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,10 +3,12 @@
 @section('content')
 
   <div class="bg-c-tan-100 pt-8 pb-4">
-    @while(have_posts()) @php(the_post())
-      {{-- @include('partials.page-header') --}}
-      @includeFirst(['partials.content-page', 'partials.content'])
-    @endwhile
+    <div class="container mx-auto">
+      @while(have_posts()) @php(the_post())
+        {{-- @include('partials.page-header') --}}
+        @includeFirst(['partials.content-page', 'partials.content'])
+      @endwhile
+    </div>
   </div>
 
 @endsection
